5b83b137aae9faffd03745024e013b399f036f92
Brendon W. <bwherry@midland.k12.ia.us>
2026-04-02 17:25:13 +0000

    gg

--- StateLookUp.cs
+++ StateLookUp.cs
@@ -1,15 +1,12 @@
 ﻿namespace knightmoves;
 using static StateList;
+
 public class StateLookUp{
 
-    public IEnumerable<string> LookUp(string search){
-        var matches = new List<string>();
-
-        foreach(string state in STATES){
-            if(state.StartsWith(search)){
-                matches.Add(state);
-            }
-        }
+    public object LookUp(string search){
+        var matches = STATES
+            .Where(state => state.StartsWith(search))
+            .ToList();
 
         return matches;
     }
