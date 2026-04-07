962824dc74f4e7ccaa1faaa55764e9d97ee1ec48
Brendon W. <bwherry@midland.k12.ia.us>
2026-04-07 11:51:34 -0500

    Update StateLookUp.cs

--- StateLookUp.cs
+++ StateLookUp.cs
@@ -3,7 +3,7 @@ using static StateList;
 
 public class StateLookUp{
 
-    public object LookUp(string search){
-       return STATES.Where(state => state.StartsWith(search));
+    public IEnumerable<string> LookUp(string search){
+        return STATES.Where(state => state.StartsWith(search));
     }
 }
